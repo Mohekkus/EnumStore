834a60660f6d2ec93f6eaf6da421c9fd8d1ce5a8
Alfiansyah <mohekkus@gmail.com>
2026-01-25 21:55:39 +0700

    Refactor: Add success method to EnumStoreLoggerImpl interface
    This commit introduces a new `success` method to the `EnumStoreLoggerImpl` interface to allow for explicit logging of successful operations.
    
    Key changes include:
    - Added `success(operation: OperationType)` with a default empty implementation to the `EnumStoreLoggerImpl` interface.

--- EnumStore/src/main/java/id/mohekkus/enumstore/logging/EnumStoreLoggerImpl.kt
+++ EnumStore/src/main/java/id/mohekkus/enumstore/logging/EnumStoreLoggerImpl.kt
@@ -3,6 +3,7 @@ package id.mohekkus.enumstore.logging
 import id.mohekkus.enumstore.operation.OperationType
 
 interface EnumStoreLoggerImpl {
+    fun success(operation: OperationType) {}
     fun error(
         operation: OperationType,
         throwable: Throwable
